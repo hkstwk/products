365fae12e7d4199ab9054e0709316e38e0f885fc
Harm Kolvoort <hkolvoort+github@gmail.com>
2016-01-20 22:49:15 +0100

    Tried couple of things with localization. pfff....

--- products/app/controllers/Products.scala
+++ products/app/controllers/Products.scala
@@ -5,10 +5,12 @@ import play.api.mvc.Controller
 import play.i18n.Messages
 import models.Product
 import play.i18n.Lang
+import play.mvc.Http.Context
 
 class Products extends Controller {
   def list = Action { implicit request =>
     val products = Product.findAll
-    Ok(views.html.html.products.list.render(products))
+    val lang = new Lang(Lang.forCode("es"))
+    Ok(views.html.html.products.list.render(products, lang))
   }
 }
