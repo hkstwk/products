25b15d421507003bd861c4007529906dc7339e50
Harm Kolvoort <hkolvoort+github@gmail.com>
2016-01-22 21:51:33 +0100

    changed model Products

--- products/app/models/Product.scala
+++ products/app/models/Product.scala
@@ -12,7 +12,7 @@ object Product {
     Product(5018206244666L, "Giant Paperclips","Giant Plain 51mm 100 pack"),
     Product(5018306332812L, "Paperclip Giant Plain","Giant Plain Pack of 10000"),
     Product(5018306312913L, "No Tear Paper Clip","No Tear Extra Large Pack of 1000"),
-    Product(5018206244611L, "Zebra Paperclips","Zebra Length 28mm Assorted 250 Pack")
+    Product(5018206244611L, "Zebra Paperclips","Zebra Length 28mm Assorted 150 Pack")
   )
 
   def findAll = products.toList.sortBy(_.ean)
